5a410d810c7de624bfb4f06358227478fbbcdc32
wudameinv <53204268+wudameinv@users.noreply.github.com>
2020-03-31 21:00:21 +0800

    Update deploy-node.ps1

--- scripts/aelf-node/deploy-node.ps1
+++ scripts/aelf-node/deploy-node.ps1
@@ -1,6 +1,6 @@
-wget https://github.com/microsoftarchive/redis/releases/download/win-3.2.100/Redis-x64-3.2.100.zip -OutFile  C:\Redis.zip ;
-Expand-Archive -Path C:\Redis.zip -DestinationPath C:\Redis ;
-(cd C:\Redis) -and (C:\Redis\redis-server.exe --service-install C:\Redis\redis.windows.conf --loglevel verbose) -and (redis-server.exe --service-start);
+# wget https://github.com/microsoftarchive/redis/releases/download/win-3.2.100/Redis-x64-3.2.100.zip -OutFile  C:\Redis.zip ;
+# Expand-Archive -Path C:\Redis.zip -DestinationPath C:\Redis ;
+# (cd C:\Redis) -and (C:\Redis\redis-server.exe --service-install C:\Redis\redis.windows.conf --loglevel verbose) -and (redis-server.exe --service-start);
 # wget https://releases.hashicorp.com/vagrant/2.2.7/vagrant_2.2.7_x86_64.msi ;
 # vagrant init hashicorp/bionic64;
 # vagrant up;
@@ -8,6 +8,10 @@ Expand-Archive -Path C:\Redis.zip -DestinationPath C:\Redis ;
 # Expand-Archive -Path C:\vagrant-redis.zip -DestinationPath C:\vagrant-redis ;
 # cd c:\vagrant-redis;
 # vagrant up;
+git clone https://github.com/ideawu/ssdb-bin.git
+git clone https://github.com/ideawu/ssdb.git
+cp ./ssdb.conf ./ssdb/ssdb.conf
+./ssdb-bin/ssdb-server-1.9.4.exe -d ./ssdb/ssdb.conf
 cd D:\a\1\s;
 mkdir -p C:\Users\Default\AppData\Local\key\ ;
 cp -r scripts\aelf-node\keys\* C:\Users\Default\AppData\Local\key\ ;
