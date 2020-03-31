f55dcf5afe4d1c7c5426558c7033b242ebe13def
wudameinv <53204268+wudameinv@users.noreply.github.com>
2020-03-31 21:37:13 +0800

    Update deploy-node.ps1

--- scripts/aelf-node/deploy-node.ps1
+++ scripts/aelf-node/deploy-node.ps1
@@ -8,18 +8,18 @@
 # Expand-Archive -Path C:\vagrant-redis.zip -DestinationPath C:\vagrant-redis ;
 # cd c:\vagrant-redis;
 # vagrant up;
-git clone https://github.com/ideawu/ssdb-bin.git;
-ls
-sleep 3
-# git clone https://github.com/ideawu/ssdb.git
-# wget https://github.com/ideawu/ssdb/archive/1.9.4.zip -o 1.9.4.zip
+# git clone https://github.com/ideawu/ssdb-bin.git;
 # ls
-# unzip 1.9.4.zip 
-# ls
-# sleep 5
-cp scripts/aelf-node/ssdb.conf ssdb-bin/ssdb.conf;
-cd ssdb-bin  
-ssdb-server-1.9.4.exe ssdb.conf
+# sleep 3
+# # git clone https://github.com/ideawu/ssdb.git
+# # wget https://github.com/ideawu/ssdb/archive/1.9.4.zip -o 1.9.4.zip
+# # ls
+# # unzip 1.9.4.zip 
+# # ls
+# # sleep 5
+# cp scripts/aelf-node/ssdb.conf ssdb-bin/ssdb.conf;
+# cd ssdb-bin  
+# ssdb-server-1.9.4.exe ssdb.conf
 netstat -an;
 cd D:\a\1\s;
 mkdir -p C:\Users\Default\AppData\Local\key\ ;
