664ab9ca54ad91666f01b361de236c632030d49b
wudameinv <53204268+wudameinv@users.noreply.github.com>
2020-03-31 23:23:10 +0800

    Update deploy-node.ps1

--- scripts/aelf-node/deploy-node.ps1
+++ scripts/aelf-node/deploy-node.ps1
@@ -31,6 +31,5 @@ ls -l aelf\ ;
 cp scripts\aelf-node\appsettings.json  aelf\appsettings.json ;
 cp scripts\aelf-node\appsettings.MainChain.TestNet.json  aelf\appsettings.MainChain.TestNet.json ;
 cd aelf/aelf
-dotnet AElf.Launcher.dll
-# #>log.txt 2>&1
-# netstat -ant ;
+dotnet AElf.Launcher.dll >log.txt 2>&1;
+netstat -ant ;
