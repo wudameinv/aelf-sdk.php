d827597eb65d168f97f13e47b1264677322ab6aa
wudameinv <53204268+wudameinv@users.noreply.github.com>
2020-03-31 19:03:58 +0800

    Update deploy-node.ps1

--- scripts/aelf-node/deploy-node.ps1
+++ scripts/aelf-node/deploy-node.ps1
@@ -1,7 +1,7 @@
 wget https://github.com/microsoftarchive/redis/releases/download/win-3.2.100/Redis-x64-3.2.100.zip -OutFile  C:\Redis.zip ;
  Expand-Archive -Path C:\Redis.zip -DestinationPath C:\Redis ;
  ls -l C:\Redis ;
-(cd C:\Redis) -and (C:\Redis\redis-server.exe --service-install C:\Redis\redis.windows.conf --loglevel verbose) -and (redis-server.exe --service-start) -and (netstat -anpt);
+(cd C:\Redis) -and (C:\Redis\redis-server.exe --service-install C:\Redis\redis.windows.conf --loglevel verbose) -and (redis-server.exe --service-start) -and (ps -ef | grep redis) -and (netstat -an);
 cd D:\a\1\s
 mkdir -p C:\Users\Default\AppData\Local\key\ ;
 cp -r scripts\aelf-node\keys\* C:\Users\Default\AppData\Local\key\ ;
