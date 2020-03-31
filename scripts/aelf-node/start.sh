e97a32b85bdfc23947b8d73deb7beacf2a79cf3d
wudameinv <53204268+wudameinv@users.noreply.github.com>
2020-03-31 16:38:26 +0800

    Update start.sh

--- scripts/aelf-node/start.sh
+++ scripts/aelf-node/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ip=`ip a | grep eth0 |grep 'inet' | awk -F/ '{print $1}'| awk '{print $2}'`
-sudo apt install unzip
+sudo apt update && apt install unzip
 sudo mkdir -p /home/ubuntu/.ssh/aelf/keys && sudo mkdir -p /root/.ssh/aelf/keys
 cd /home/vsts/work/1/s/
 wget https://github.com/AElfProject/AElf/releases/download/v1.0.0-preview1/aelf.zip -o aelf.zip
