7b881a1b962cb8f7d15af58b0185fb2c1a19a0ef
wudameinv <53204268+wudameinv@users.noreply.github.com>
2020-03-31 16:50:16 +0800

    Update start.sh

--- scripts/aelf-node/start.sh
+++ scripts/aelf-node/start.sh
@@ -2,7 +2,7 @@
 ip=`ip a | grep eth0 |grep 'inet' | awk -F/ '{print $1}'| awk '{print $2}'`
 sudo apt update && apt install unzip
 sudo mkdir -p /home/ubuntu/.ssh/aelf/keys && sudo mkdir -p /root/.ssh/aelf/keys
-cd /home/vsts/work/1/s/
+cd ../../
 wget https://github.com/AElfProject/AElf/releases/download/v1.0.0-preview1/aelf.zip
 sudo unzip aelf.zip
 sudo cp scripts/aelf-node/keys/*.json /home/ubuntu/.ssh/aelf/keys/
@@ -10,5 +10,6 @@ sudo cp scripts/aelf-node/keys/*.json /root/.ssh/aelf/keys/
 sudo cp scripts/aelf-node/app* aelf/
 echo "start node"
 cd aelf && sudo dotnet AElf.Launcher.dll >/dev/null 2>&1 &
+sleep 30
 height=`curl -s http://$ip:8001/api/blockChain/blockHeight`
 echo "height is $height"
