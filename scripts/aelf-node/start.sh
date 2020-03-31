3504d19c472afc844f90eba9f4f7d9d6357a8a3b
wudameinv <huihui.wu@aelf.io>
2020-03-31 07:11:29 +0000

    test

--- scripts/aelf-node/start.sh
+++ scripts/aelf-node/start.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-dir=`pwd`
 ip=`ip a | grep eth0 |grep 'inet' | awk -F/ '{print $1}'| awk '{print $2}'`
-# sudo sed -i "s/172.31.8.57/$ip/g" appsettings.json 
-sudo docker pull aelf/node:dev
-sudo docker run -itd --name aelf-node-test -v $dir:/opt/node -v $dir/keys:/root/.local/share/aelf/keys -p 8000:8000 -p 6800:6800 -w /opt/node aelf/node:dev dotnet /app/AElf.Launcher.dll
-sleep 30
+#sudo docker run -itd --name aelf-node-test -v $dir:/opt/node -v $dir/keys:/root/.local/share/aelf/keys -p 8000:8000 -p 6800:6800 -w /opt/node aelf/node:dev dotnet /app/AElf.Launcher.dll
+apt install unzip -y
+mkdir -p /home/ubuntu/.ssh/aelf/keys
+mkdir -p /root/.ssh/aelf/keys
+cd ../../
+wget https://github.com/AElfProject/AElf/releases/download/v1.0.0-preview1/aelf.zip && unzip aelf.zip
+cp scripts/aelf-node/keys/SD6BXDrKT2syNd1WehtPyRo3dPBiXqfGUj8UJym7YP9W9RynM.json  /home/ubuntu/.ssh/aelf/keys
+cp scripts/aelf-node/keys/SD6BXDrKT2syNd1WehtPyRo3dPBiXqfGUj8UJym7YP9W9RynM.json  /root/.ssh/aelf/keys
+cp scripts/aelf-node/app* aelf/
+echo "start node"
+cd aelf && dotnet AElf.Launcher.dll >/dev/null 2>&1 &
 height=`curl -s http://$ip:8001/api/blockChain/blockHeight`
 echo "height is $height"
