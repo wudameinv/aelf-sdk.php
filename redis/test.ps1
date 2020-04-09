5144f8e6b1b87861630be917bc3f864ef888c4d0
wudameinv <53204268+wudameinv@users.noreply.github.com>
2020-04-10 02:09:53 +0800

    Update test.ps1

--- redis/test.ps1
+++ redis/test.ps1
@@ -1,4 +1,6 @@
 docker build -f redis/Dockerfile -t redis:3.2 .
 docker images
-docker run -d --name redis -p 6379:6379 redis:3.2
+docker run -itd --name redis -p 6379:6379 redis:3.2 "c:\redis\redis-server.exe c:\redis\redis.windows.conf"
+sleep 30
 docker ps -a
+netstat -an
