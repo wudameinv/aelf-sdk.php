9746c6404da82aa83c1b43bcf56a2e6d990dec9a
wudameinv <53204268+wudameinv@users.noreply.github.com>
2020-04-10 02:30:29 +0800

    Update test.ps1

--- redis/test.ps1
+++ redis/test.ps1
@@ -1,6 +1,6 @@
 # docker build -f redis/Dockerfile -t redis:3.2 .
 docker images
-docker run -itd --name redis -p 6379:6379 amd64/redis
+docker run -id --name redis -p 6379:6379 smithyuk/windows-redis:latest
 sleep 30
 docker ps -a
 netstat -an
