81646b64f9e37b7daf5286ac39bdc0b4ad208d04
wudameinv <53204268+wudameinv@users.noreply.github.com>
2020-04-09 22:16:03 +0800

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,8 +1,9 @@
 docker build -t aelf/node:test  .
 docker images 
-docker run -d --name aelf aelf/node:test
+docker run -d --name aelf -p 8001:8001 aelf/node:test
 docker ps
 echo "sleep 30"
 echo "docker logs aelf"
 docker logs aelf
-
+pwd
+ls
