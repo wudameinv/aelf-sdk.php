a5c9240a037e79ff954682ce35f509147a299bc9
wudameinv <53204268+wudameinv@users.noreply.github.com>
2020-04-10 12:21:31 +0800

    Update test.ps1

--- redis/test.ps1
+++ redis/test.ps1
@@ -1,5 +1,5 @@
 docker build -f redis/Dockerfile -t windows-redis .
-docker run  -itd windows-redis
+docker run  -itd windows-redis "C:\ProgramData\chocolatey\lib\redis-64\redis-server.exe c:\redis.conf"
 docker ps
 docker images
 
