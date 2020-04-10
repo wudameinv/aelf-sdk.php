c379a75ee414c9d7d4725560c22c65285229e0ec
wudameinv <53204268+wudameinv@users.noreply.github.com>
2020-04-10 11:49:53 +0800

    Update test.ps1

--- redis/test.ps1
+++ redis/test.ps1
@@ -1,5 +1,5 @@
 docker build -f redis/Dockerfile -t smithyuk/windows-redis .
-docker run -p 6379:6379 -id smithyuk/windows-redis
+docker run -p 6379:6379 -itd smithyuk/windows-redis
 docker images
 sleep 30
 docker ps -a
