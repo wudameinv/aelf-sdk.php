e161ebc1eb28853d41bfc3832da2b2304939cc7c
wudameinv <53204268+wudameinv@users.noreply.github.com>
2020-04-10 20:39:53 +0800

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,13 +1,13 @@
 docker build -t aelf/node:test  .
-docker run -d --name aelf -p 8001:8001  aelf/node:test 
-# AElf.Launcher.dll
+# docker run -d --name aelf -p 8001:8001  aelf/node:test 
+# # AElf.Launcher.dll
 
-echo "sleep 60"
-sleep  60
-docker ps -a 
+# echo "sleep 60"
+# sleep  60
+# docker ps -a 
 
-echo "docker logs aelf"
-docker logs aelf
-pwd
-dir c:/
+# echo "docker logs aelf"
+# docker logs aelf
+# pwd
+# dir c:/
 
