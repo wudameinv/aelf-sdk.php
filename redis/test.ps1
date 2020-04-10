08a8de94b5c71bcda8de5a131b6beea713282239
wudameinv <53204268+wudameinv@users.noreply.github.com>
2020-04-10 20:31:23 +0800

    Update test.ps1

--- redis/test.ps1
+++ redis/test.ps1
@@ -1,13 +1,13 @@
 docker build -f redis/Dockerfile -t windows-redis .
-docker ps -a
-docker run -itd --name redis  windows-redis 
-docker ps -a 
-echo "sleep 30"
-sleep 30
+# docker ps -a
+# docker run -itd --name redis  windows-redis 
+# docker ps -a 
+# echo "sleep 30"
+# sleep 30
 
-docker ps
-echo "Web server running at:"
-docker inspect -f "{{ .NetworkSettings.Networks.nat.IPAddress }}" redis
+# docker ps
+# echo "Web server running at:"
+# docker inspect -f "{{ .NetworkSettings.Networks.nat.IPAddress }}" redis
 # docker run  -itd windows-redis "C:\ProgramData\chocolatey\lib\redis-64\redis-server.exe"
 # docker ps
 # docker images
