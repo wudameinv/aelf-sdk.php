48ba069705dff7f691815d174efae0c23f33773b
wudameinv <53204268+wudameinv@users.noreply.github.com>
2020-04-10 15:52:17 +0800

    Update test.ps1

--- redis/test.ps1
+++ redis/test.ps1
@@ -1,5 +1,4 @@
 docker build -f redis/Dockerfile -t windows-redis .
-docker rm -f windows-redis
 docker run -d --name redis -p 6379:6379  windows-redis
 echo "Web server running at:"
 docker inspect -f "{{ .NetworkSettings.Networks.nat.IPAddress }}" redis
