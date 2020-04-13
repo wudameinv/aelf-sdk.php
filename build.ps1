04f75fe9f39570d71cee57a1c0c5233ce7e3b836
wudameinv <53204268+wudameinv@users.noreply.github.com>
2020-04-13 18:11:06 +0800

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,9 +1,4 @@
 docker build -t aelf/node:test  .
-Stop-Service docker
-Stop-service hns
-Start-service hns
-Start-Service docker
-docker network prune
 docker run -d --name aelf -p 8001:8001  aelf/node:test 
 sleep 30
 docker logs aelf
