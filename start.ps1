86e2512bbf2a56191057284ebca8a60ab5e639c5
wudameinv <huihui.wu@aelf.io>
2020-04-14 08:00:29 +0000

    test

--- start.ps1
+++ start.ps1
@@ -1,2 +1,10 @@
-C:\\ServiceMonitor.exe w3svc 
+wget http://18.203.235.132:8000/aelf-node.zip  -OutFile  c:\aelf.zip
+Expand-Archive -Path c:\aelf.zip -DestinationPath c:\aelf
+dir c:\aelf\
+
+dir c:\
+./dotnet-install.ps1 -InstallDir '~/.dotnet' -Version '3.1.102' 
+mkdir -p C:\Users\ContainerAdministrator\AppData\Local\aelf\keys
+cp c:/appsettings.json c:/aelf/aelf-node/
+cp c:/*.json C:/Users/ContainerAdministrator/AppData/Local/aelf/keys/
 dotnet c:/aelf/aelf-node/AElf.Launcher.dll
