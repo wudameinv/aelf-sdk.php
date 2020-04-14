fc9fe3a88f8f7cead531eaf6f7083f86b9209146
wudameinv <huihui.wu@aelf.io>
2020-04-14 11:12:03 +0000

    test

--- scripts/aelf-node/deploy-node.ps1
+++ scripts/aelf-node/deploy-node.ps1
@@ -1,18 +1,19 @@
-git clone https://github.com/ideawu/ssdb-bin.git
-cp C:/__w/1/s/scripts/aelf-node/ssdb.conf ssdb-bin/ssdb.conf
-cd ssdb-bin  
-cat ssdb.conf
-./ssdb-server-1.9.4.exe -d ssdb.conf  
-cat log.txt
-netstat -an
+#git clone https://github.com/ideawu/ssdb-bin.git
+#cp C:/__w/1/s/scripts/aelf-node/ssdb.conf ssdb-bin/ssdb.conf
+#cd ssdb-bin  
+#cat ssdb.conf
+#./ssdb-server-1.9.4.exe -d ssdb.conf  
+#cat log.txt
+#netstat -an
 mkdir -p C:\Users\VssAdministrator\AppData\Local\aelf\keys
 cp -r scripts\aelf-node\keys\* C:\Users\VssAdministrator\AppData\Local\aelf\keys;
 ls -l C:\Users\VssAdministrator\AppData\Local\aelf\keys ;
-wget https://github.com/AElfProject/AElf/releases/download/v1.0.0-preview1/aelf.zip -OutFile  aelf.zip ;
+#wget https://github.com/AElfProject/AElf/releases/download/v1.0.0-preview1/aelf.zip -OutFile  aelf.zip ;
+wget wget http://18.203.235.132:8000/aelf-node.zip -OutFile  aelf.zip ; 
 Expand-Archive -Path aelf.zip -DestinationPath aelf ;
 ls -l aelf\ ;
-cp scripts\aelf-node\appsettings.json  aelf\aelf\appsettings.json ;
-cp scripts\aelf-node\appsettings.MainChain.TestNet.json  aelf\aelf\appsettings.MainChain.TestNet.json ;
-cd aelf/aelf
+cp scripts\aelf-node\appsettings.json  aelf\aelf-node\appsettings.json ;
+cp scripts\aelf-node\appsettings.MainChain.TestNet.json  aelf\aelf-node\appsettings.MainChain.TestNet.json ;
+cd aelf/aelf-node
 dotnet AElf.Launcher.dll ;
 netstat -ant ;
