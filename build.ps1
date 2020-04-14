fb30f02082e5b3069d080a60e73e51c48506d998
wudameinv <53204268+wudameinv@users.noreply.github.com>
2020-04-14 17:48:42 +0800

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -2,7 +2,7 @@
 # docker history  mcr.microsoft.com/dotnet/framework/aspnet:4.8-windowsservercore-ltsc2019 --format "table {{.ID}}\t{{.CreatedBy}}" --no-trunc
 #docker build -t aelf/node:test  .
 docker run -itd --name aelf  mcr.microsoft.com/dotnet/framework/aspnet:4.8-windowsservercore-ltsc2019
-docker exec -t aelf bash -c "mkdir -p c:\aelf\aelf"
+docker exec -t aelf  "mkdir -p c:\aelf\aelf"
 # winpty docker exec -it aelf powershell.exe -c 'dotnet c:/aelf/aelf-node/AElf.Launcher.dll'
 # #dotnet c:/aelf/aelf-node/AElf.Launcher.dll
 # sleep 30
