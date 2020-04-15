57ec529dfaa8c05dbe07f44b3d0a855c86163713
wudameinv <53204268+wudameinv@users.noreply.github.com>
2020-04-15 12:03:59 +0800

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,6 +1,6 @@
 docker build -t aelf/node:test .
 # docker run -itd aelf/node:test dotnet c:/aelf/aelf-node/AElf.Launcher.dll
-docker run -it -p 8001:8001 -p 88:80   aelf/node:test 
+winpty docker run -it -p 8001:8001 -p 88:80   aelf/node:test bash
 
 docker ps
 sleep 30
