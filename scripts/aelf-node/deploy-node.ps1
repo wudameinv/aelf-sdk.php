427dfbaf9f9e38985575b2e95863429c9e916a14
wudameinv <53204268+wudameinv@users.noreply.github.com>
2020-04-15 17:25:23 +0800

    Update deploy-node.ps1

--- scripts/aelf-node/deploy-node.ps1
+++ scripts/aelf-node/deploy-node.ps1
@@ -11,10 +11,10 @@ cp -r scripts\aelf-node\keys\* C:\Users\VssAdministrator\AppData\Local\aelf\keys
 #wget https://github.com/AElfProject/AElf/releases/download/v1.0.0-preview1/aelf.zip -OutFile  aelf.zip ;
 wget http://18.203.235.132:8000/aelf-node.zip -OutFile  aelf.zip ; 
 Expand-Archive -Path aelf.zip -DestinationPath aelf ;
-# ls -l aelf\aelf-node\ ;
+ls -l aelf\ ;
 cp scripts\aelf-node\appsettings.json  aelf\aelf-node\appsettings.json ;
 cp scripts\aelf-node\appsettings.MainChain.TestNet.json  aelf\aelf-node\appsettings.MainChain.TestNet.json ;
-cd aelf/aelf-node
+cd aelf/aelf-node ;
 # Start-Job -ScriptBlock {& dotnet AElf.Launcher.dll} ;
 # cd aelf/aelf-node ;
 # dotnet AElf.Launcher.dll ;
