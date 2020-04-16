aad753ec0e01b92d85b8fe36c59240ca14e1349d
wudameinv <53204268+wudameinv@users.noreply.github.com>
2020-04-16 17:59:17 +0800

    Update test2.ps1

--- test2.ps1
+++ test2.ps1
@@ -1,12 +1,12 @@
 $progressPreference = 'silentlyContinue'
 mkdir 'c:\Program Files\Linux Containers'
-
-Invoke-WebRequest -UseBasicParsing -OutFile linuxkit.zip https://github.com/friism/linuxkit/releases/download/preview-1/linuxkit.zip
-Expand-Archive linuxkit.zip -DestinationPath 'c:\Program Files\Linux Containers\'
+Invoke-WebRequest -UseBasicParsing -OutFile preview-1.zip https://github.com/friism/linuxkit/archive/preview-1.zip
+# Invoke-WebRequest -UseBasicParsing -OutFile linuxkit.zip https://github.com/friism/linuxkit/releases/download/preview-1/linuxkit.zip
+Expand-Archive preview-1.zip -DestinationPath 'c:\Program Files\Linux Containers\'
 ls -l 'c:\Program Files\Linux Containers\'
 rm linuxkit.zip
 Invoke-WebRequest -UseBasicParsing -OutFile dockerd.exe https://master.dockerproject.org/windows/x86_64/dockerd.exe
 $Env:LCOW_SUPPORTED=1
 $env:LCOW_API_PLATFORM_IF_OMITTED="linux"
 # .\dockerd.exe -D --experimental -H "npipe:////./pipe//docker_lcow" --data-root c:\lcow
-docker  -H "npipe:////./pipe//docker_lcow" run -itd  -p 8001:8000   aelf/node:latest
+# docker  -H "npipe:////./pipe//docker_lcow" run -itd  -p 8001:8000   aelf/node:latest
