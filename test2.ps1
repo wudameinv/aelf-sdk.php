3f45f0189477450bad9e1e7927e0c2ee5710c0eb
wudameinv <53204268+wudameinv@users.noreply.github.com>
2020-04-16 17:13:06 +0800

    Update test2.ps1

--- test2.ps1
+++ test2.ps1
@@ -3,7 +3,7 @@ mkdir 'c:\ProgramFiles\Linux Containers'
 
 Invoke-WebRequest -UseBasicParsing -OutFile linuxkit.zip https://github.com/friism/linuxkit/releases/download/preview-1/linuxkit.zip
 Expand-Archive linuxkit.zip -DestinationPath 'c:\ProgramFiles\Linux Containers\'
-ls -l c:\ProgramFiles\Linux Containers\
+ls -l 'c:\ProgramFiles\Linux Containers\'
 rm linuxkit.zip
 Invoke-WebRequest -UseBasicParsing -OutFile dockerd.exe https://master.dockerproject.org/windows/x86_64/dockerd.exe
 $Env:LCOW_SUPPORTED=1
