bf473383c4c2b46539f0df23d191dc7e6ab57363
wudameinv <53204268+wudameinv@users.noreply.github.com>
2020-04-16 16:43:58 +0800

    Create test2.ps1

---
+++ test2.ps1
@@ -0,0 +1,12 @@
+$progressPreference = 'silentlyContinue'
+mkdir "$Env:ProgramFiles\Linux Containers”
+
+Invoke-WebRequest -UseBasicParsing -OutFile linuxkit.zip https://github.com/friism/linuxkit/releases/download/preview-1/linuxkit.zip
+
+Expand-Archive linuxkit.zip -DestinationPath "$Env:ProgramFiles\Linux Containers\."
+rm linuxkit.zip
+Invoke-WebRequest -UseBasicParsing -OutFile dockerd.exe https://master.dockerproject.org/windows/x86_64/dockerd.exe
+$Env:LCOW_SUPPORTED=1
+$env:LCOW_API_PLATFORM_IF_OMITTED="linux"
+.\dockerd.exe -D --experimental -H "npipe:////./pipe//docker_lcow" --data-root c:\lcow
+docker -H "npipe:////./pipe//docker_lcow" run -ti busybox sh
