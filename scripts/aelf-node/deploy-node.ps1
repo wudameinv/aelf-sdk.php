91b491ee5b0278739573e9e448e58733b3d2ee91
jinpeng wang <jason.wang@hoopox.com>
2020-04-20 21:34:50 +0800

    test

--- scripts/aelf-node/deploy-node.ps1
+++ scripts/aelf-node/deploy-node.ps1
@@ -7,13 +7,13 @@ ls -l aelf\
 cp scripts\aelf-node\appsettings.json  aelf\aelf-node\appsettings.json
 cp scripts\aelf-node\appsettings.MainChain.TestNet.json  aelf\aelf-node\appsettings.MainChain.TestNet.json
 cd aelf/aelf-node 
-$job = Start-Job -ScriptBlock { pwd; }
+$job = Start-Job -ScriptBlock { pwd; } -WorkingDirectory "D:\a\1\s\aelf\aelf-node"
 Wait-Job $job
 Receive-Job -Job $job
-Start-Job -ScriptBlock { dotnet AElf.Launcher.dll; }
+Start-Job -ScriptBlock { cd; dotnet AElf.Launcher.dll; } -WorkingDirectory "D:\a\1\s\aelf\aelf-node"
 sleep 60
 Get-Job
-#netstat -an
+netstat -an
 cd D:\a\1\s\
 cp scripts/aelf-node/php.ini /c/tools/php/php.ini
 php -c /c/tools/php/php.ini
