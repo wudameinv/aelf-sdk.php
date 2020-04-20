8819793a3545f2b8615bdb4616f033fc9743b23f
jinpeng wang <jason.wang@hoopox.com>
2020-04-20 18:08:49 +0800

    test

--- scripts/aelf-node/deploy-node.ps1
+++ scripts/aelf-node/deploy-node.ps1
@@ -6,19 +6,21 @@
 #cat log.txt
 #netstat -an
 mkdir -p C:\Users\VssAdministrator\AppData\Local\aelf\keys
-cp -r scripts\aelf-node\keys\* C:\Users\VssAdministrator\AppData\Local\aelf\keys;
+cp -r scripts\aelf-node\keys\* C:\Users\VssAdministrator\AppData\Local\aelf\keys
 # ls -l C:\Users\VssAdministrator\AppData\Local\aelf\keys ;
-#wget https://github.com/AElfProject/AElf/releases/download/v1.0.0-preview1/aelf.zip -OutFile  aelf.zip ;
-wget http://18.203.235.132:8000/aelf-node.zip -OutFile  aelf.zip ; 
-Expand-Archive -Path aelf.zip -DestinationPath aelf ;
-ls -l aelf\ ;
-cp scripts\aelf-node\appsettings.json  aelf\aelf-node\appsettings.json ;
-cp scripts\aelf-node\appsettings.MainChain.TestNet.json  aelf\aelf-node\appsettings.MainChain.TestNet.json ;
-cd aelf/aelf-node ;
-# Start-Job -ScriptBlock {& dotnet AElf.Launcher.dll} ;
-# # cd aelf/aelf-node ;
-# dotnet AElf.Launcher.dll ;
-# Start-Job -ScriptBlock {ping 8.8.8.8} ;
+#wget https://github.com/AElfProject/AElf/releases/download/v1.0.0-preview1/aelf.zip -OutFile  aelf.zip
+wget http://18.203.235.132:8000/aelf-node.zip -OutFile  aelf.zip 
+Expand-Archive -Path aelf.zip -DestinationPath aelf
+ls -l aelf\
+cp scripts\aelf-node\appsettings.json  aelf\aelf-node\appsettings.json
+cp scripts\aelf-node\appsettings.MainChain.TestNet.json  aelf\aelf-node\appsettings.MainChain.TestNet.json
+cd aelf/aelf-node 
+# Start-Job -ScriptBlock {& dotnet AElf.Launcher.dll} 
+Start-Job -ScriptBlock {dotnet AElf.Launcher.dll}
+Get-Job
+# # cd aelf/aelf-node 
+# dotnet AElf.Launcher.dll 
+# Start-Job -ScriptBlock {ping 8.8.8.8} 
 # dotnet c:\aelf\aelf-node\AElf.Launcher.dll
 # sleep 10
 # Get-Job
@@ -28,6 +30,6 @@ cd aelf/aelf-node ;
 # Get-Job
 # C:\Users\ContainerAdministrator\.dotnet\dotnet.exe
 # cd aelf/aelf-node
-# dotnet AElf.Launcher.dll ;
+# dotnet AElf.Launcher.dll 
 ##Start-Job { & dotnet AElf.Launcher.dll }
-#netstat -ant ;
+#netstat -ant 
