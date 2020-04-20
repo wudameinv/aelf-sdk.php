ef540342e7967e0191435e0df304c41708a0b283
jinpeng wang <jason.wang@hoopox.com>
2020-04-20 18:47:14 +0800

    test

--- scripts/aelf-node/deploy-node.ps1
+++ scripts/aelf-node/deploy-node.ps1
@@ -1,36 +1,23 @@
 pwd
-#git clone https://github.com/ideawu/ssdb-bin.git
-#cp C:/__w/1/s/scripts/aelf-node/ssdb.conf ssdb-bin/ssdb.conf
-#cd ssdb-bin  
-#cat ssdb.conf
-#./ssdb-server-1.9.4.exe -d ssdb.conf  
-#cat log.txt
-#netstat -an
 mkdir -p C:\Users\VssAdministrator\AppData\Local\aelf\keys
 cp -r scripts\aelf-node\keys\* C:\Users\VssAdministrator\AppData\Local\aelf\keys
-# ls -l C:\Users\VssAdministrator\AppData\Local\aelf\keys ;
-#wget https://github.com/AElfProject/AElf/releases/download/v1.0.0-preview1/aelf.zip -OutFile  aelf.zip
 wget http://18.203.235.132:8000/aelf-node.zip -OutFile  aelf.zip 
 Expand-Archive -Path aelf.zip -DestinationPath aelf
 ls -l aelf\
 cp scripts\aelf-node\appsettings.json  aelf\aelf-node\appsettings.json
 cp scripts\aelf-node\appsettings.MainChain.TestNet.json  aelf\aelf-node\appsettings.MainChain.TestNet.json
 cd aelf/aelf-node 
-# Start-Job -ScriptBlock {& dotnet AElf.Launcher.dll} 
+Get-Job
 Start-Job -ScriptBlock {dotnet AElf.Launcher.dll}
 Get-Job
-# # cd aelf/aelf-node 
-# dotnet AElf.Launcher.dll 
-# Start-Job -ScriptBlock {ping 8.8.8.8} 
-# dotnet c:\aelf\aelf-node\AElf.Launcher.dll
-# sleep 10
-# Get-Job
-# netstat -an
-# sleep 30 
-# netstat -an
-# Get-Job
-# C:\Users\ContainerAdministrator\.dotnet\dotnet.exe
-# cd aelf/aelf-node
-# dotnet AElf.Launcher.dll 
-##Start-Job { & dotnet AElf.Launcher.dll }
-#netstat -ant 
+sleep 60
+Get-Job
+netstat -an
+cd D:\a\1\s\
+cp scripts/aelf-node/php.ini /c/tools/php/php.ini
+php -c /c/tools/php/php.ini
+php -r "copy('https://install.phpcomposer.com/installer', 'composer-setup.php');"
+php composer-setup.php
+php composer.phar config -g repo.packagist composer https://packagist.phpcomposer.com
+php composer.phar install --no-interaction --prefer-dist
+php phpunit.phar --bootstrap bootstrap.php tests/AElfTest.php 
