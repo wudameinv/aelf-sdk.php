8c8d7a1cafeed8cac8964e3f17f256a26efab638
wudameinv <53204268+wudameinv@users.noreply.github.com>
2020-04-21 19:26:26 +0800

    Update deploy-node.ps1

--- scripts/aelf-node/deploy-node.ps1
+++ scripts/aelf-node/deploy-node.ps1
@@ -6,19 +6,27 @@
 # Start-Job -ScriptBlock { cd c:\redis; redis-server; } 
 # sleep 30
 # netstat -an
-mkdir -p C:\Users\VssAdministrator\AppData\Local\aelf\keys
-cp -r scripts\aelf-node\keys\* C:\Users\VssAdministrator\AppData\Local\aelf\keys;
-wget http://18.203.235.132:8000/aelf-node.zip -OutFile  aelf.zip ; 
-Expand-Archive -Path aelf.zip -DestinationPath aelf ;
-cp scripts\aelf-node\appsettings.json  aelf\aelf-node\appsettings.json ;
-cp scripts\aelf-node\appsettings.MainChain.TestNet.json  aelf\aelf-node\appsettings.MainChain.TestNet.json ;
-cd aelf/aelf-node 
-$job1 = Start-Job -ScriptBlock { cd D:\a\1\s\aelf\aelf-node; pwd; }
-Wait-Job $job
-Receive-Job -Job $job1
-$job2 = Start-Job -ScriptBlock { cd D:\a\1\s\aelf\aelf-node; dotnet AElf.Launcher.dll; } 
-sleep 30
-Receive-Job -Job $job2
+cp scripts/aelf-node/php.ini c:\tools\php\php.ini
+php -c c:\tools\php\php.ini
+wget https://install.phpcomposer.com/installer
+mv installer composer-setup.php
+php composer-setup.php
+php composer.phar config -g repo.packagist composer https://packagist.phpcomposer.com
+php composer.phar install --no-interaction --prefer-dist
+
+# mkdir -p C:\Users\VssAdministrator\AppData\Local\aelf\keys
+# cp -r scripts\aelf-node\keys\* C:\Users\VssAdministrator\AppData\Local\aelf\keys;
+# wget http://18.203.235.132:8000/aelf-node.zip -OutFile  aelf.zip ; 
+# Expand-Archive -Path aelf.zip -DestinationPath aelf ;
+# cp scripts\aelf-node\appsettings.json  aelf\aelf-node\appsettings.json ;
+# cp scripts\aelf-node\appsettings.MainChain.TestNet.json  aelf\aelf-node\appsettings.MainChain.TestNet.json ;
+# cd aelf/aelf-node 
+# $job1 = Start-Job -ScriptBlock { cd D:\a\1\s\aelf\aelf-node; pwd; }
+# Wait-Job $job
+# Receive-Job -Job $job1
+# $job2 = Start-Job -ScriptBlock { cd D:\a\1\s\aelf\aelf-node; dotnet AElf.Launcher.dll; } 
+# sleep 30
+# Receive-Job -Job $job2
 
 netstat -an
 sleep 30 
