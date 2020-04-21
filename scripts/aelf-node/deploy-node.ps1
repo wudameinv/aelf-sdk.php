416fde85b50ca3b62272e36996b7c26681d30470
wudameinv <53204268+wudameinv@users.noreply.github.com>
2020-04-21 20:56:29 +0800

    Update deploy-node.ps1

--- scripts/aelf-node/deploy-node.ps1
+++ scripts/aelf-node/deploy-node.ps1
@@ -1,10 +1,11 @@
-cp scripts\aelf-node\php.ini c:\tools\php\php.ini
-php -c c:\tools\php\php.ini
-# php -r " eval('?>' .file_get_contents('https://getcomposer.org/installer'));"
-php -r "copy('https://install.phpcomposer.com/installer', 'composer-setup.php');"
-php composer-setup.php
-php composer.phar config -g repo.packagist composer https://packagist.phpcomposer.com
-# php composer.phar install --no-interaction --prefer-dist
-composer install
-php phpunit.phar --bootstrap bootstrap.php tests/AElfTest.php 
+# cp scripts\aelf-node\php.ini c:\tools\php\php.ini
+# php -c c:\tools\php\php.ini
+# # php -r " eval('?>' .file_get_contents('https://getcomposer.org/installer'));"
+# php -r "copy('https://install.phpcomposer.com/installer', 'composer-setup.php');"
+# php composer-setup.php
+# php composer.phar config -g repo.packagist composer https://packagist.phpcomposer.com
+# # php composer.phar install --no-interaction --prefer-dist
+# composer install
+# php phpunit.phar --bootstrap bootstrap.php tests/AElfTest.php 
 
+php phpunit.phar --bootstrap bootstrap.php tests/AElfTest.php
