f7657e9cdf66a53136aea3762638aaac1d7f226e
wudameinv <53204268+wudameinv@users.noreply.github.com>
2020-04-21 19:36:36 +0800

    Update deploy-node.ps1

--- scripts/aelf-node/deploy-node.ps1
+++ scripts/aelf-node/deploy-node.ps1
@@ -12,7 +12,7 @@ php -r "copy('https://install.phpcomposer.com/installer', 'composer-setup.php');
 php composer-setup.php
 # php composer.phar config -g repo.packagist composer https://packagist.phpcomposer.com
 php composer.phar install --no-interaction --prefer-dist
-
+php phpunit.phar --bootstrap bootstrap.php tests/AElfTest.php 
 # mkdir -p C:\Users\VssAdministrator\AppData\Local\aelf\keys
 # cp -r scripts\aelf-node\keys\* C:\Users\VssAdministrator\AppData\Local\aelf\keys;
 # wget http://18.203.235.132:8000/aelf-node.zip -OutFile  aelf.zip ; 
