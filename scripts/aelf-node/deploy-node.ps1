3cf69ce9c7c20a2ee2c43add7d0e014288ea08a6
wudameinv <53204268+wudameinv@users.noreply.github.com>
2020-04-21 19:52:05 +0800

    Update deploy-node.ps1

--- scripts/aelf-node/deploy-node.ps1
+++ scripts/aelf-node/deploy-node.ps1
@@ -8,7 +8,8 @@
 # netstat -an
 cp scripts/aelf-node/php.ini c:\tools\php\php.ini
 php -c c:\tools\php\php.ini
-php -r "copy('https://install.phpcomposer.com/installer', 'composer-setup.php');"
+php -r " eval('?>' .file_get_contents('https://getcomposer.org/installer'));"
+# php -r "copy('https://install.phpcomposer.com/installer', 'composer-setup.php');"
 php composer-setup.php
 # php composer.phar config -g repo.packagist composer https://packagist.phpcomposer.com
 # php composer.phar install --no-interaction --prefer-dist
