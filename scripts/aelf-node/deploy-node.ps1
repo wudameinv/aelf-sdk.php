a10c9d2bbb39a73b31e55d2f938716d4bb32f268
wudameinv <53204268+wudameinv@users.noreply.github.com>
2020-04-21 15:16:42 +0800

    Update deploy-node.ps1

--- scripts/aelf-node/deploy-node.ps1
+++ scripts/aelf-node/deploy-node.ps1
@@ -13,8 +13,9 @@ Expand-Archive -Path aelf.zip -DestinationPath aelf ;
 cp scripts\aelf-node\appsettings.json  aelf\aelf-node\appsettings.json ;
 cp scripts\aelf-node\appsettings.MainChain.TestNet.json  aelf\aelf-node\appsettings.MainChain.TestNet.json ;
 cd aelf/aelf-node 
-# # $job = Start-Job -ScriptBlock { cd D:\a\1\s\aelf\aelf-node; pwd; }
-# # Wait-Job $job
-# # Receive-Job -Job $job
+$job = Start-Job -ScriptBlock { cd D:\a\1\s\aelf\aelf-node; pwd; }
+Wait-Job $job
+Receive-Job -Job $job
 Start-Job -ScriptBlock { cd D:\a\1\s\aelf\aelf-node; dotnet AElf.Launcher.dll; } 
+sleep 30
 netstat -an
