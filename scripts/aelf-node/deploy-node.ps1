ca853cc5672dc2006ecce10c77379f0027edf00b
wudameinv <53204268+wudameinv@users.noreply.github.com>
2020-04-21 19:36:23 +0800

    Update deploy-node.ps1

--- scripts/aelf-node/deploy-node.ps1
+++ scripts/aelf-node/deploy-node.ps1
@@ -10,7 +10,7 @@ cp scripts/aelf-node/php.ini c:\tools\php\php.ini
 php -c c:\tools\php\php.ini
 php -r "copy('https://install.phpcomposer.com/installer', 'composer-setup.php');"
 php composer-setup.php
-php composer.phar config -g repo.packagist composer https://packagist.phpcomposer.com
+# php composer.phar config -g repo.packagist composer https://packagist.phpcomposer.com
 php composer.phar install --no-interaction --prefer-dist
 
 # mkdir -p C:\Users\VssAdministrator\AppData\Local\aelf\keys
