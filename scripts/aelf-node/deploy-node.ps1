ab14586164be949f7fa2ce86b78215a7b6389c91
wudameinv <53204268+wudameinv@users.noreply.github.com>
2020-04-22 11:30:12 +0800

    Update deploy-node.ps1

--- scripts/aelf-node/deploy-node.ps1
+++ scripts/aelf-node/deploy-node.ps1
@@ -2,20 +2,21 @@ wget https://github.com/microsoftarchive/redis/releases/download/win-3.2.100/Red
 Expand-Archive -Path redis.zip -DestinationPath redis1 ;
 ls -l D:\a\1\s\redis1
 $job1 = Start-Job -ScriptBlock { cd D:\a\1\s\redis1; .\redis-server.exe; }
-netstat -an
-get-job
+# netstat -an
+# get-job
 sleep 30
-Receive-Job -Job $job1
-get-job
+# Receive-Job -Job $job1
+# get-job
 netstat -an
-# mkdir -p C:\Users\VssAdministrator\AppData\Local\aelf\keys
-# cp -r scripts\aelf-node\keys\* C:\Users\VssAdministrator\AppData\Local\aelf\keys;
+mkdir -p C:\Users\VssAdministrator\AppData\Local\aelf\keys
+cp -r scripts\aelf-node\keys\* C:\Users\VssAdministrator\AppData\Local\aelf\keys;
 # wget http://18.203.235.132:8000/aelf-node.zip -OutFile  aelf.zip ; 
-# Expand-Archive -Path aelf.zip -DestinationPath aelf ;
-# cp scripts\aelf-node\appsettings.json  aelf\aelf-node\appsettings.json ;
-# cp scripts\aelf-node\appsettings.MainChain.TestNet.json  aelf\aelf-node\appsettings.MainChain.TestNet.json ;
-# cd aelf/aelf-node 
-# $job2 = Start-Job -ScriptBlock { cd D:\a\1\s\aelf\aelf-node; dotnet AElf.Launcher.dll; } 
-# sleep 60
-# cd D:\a\1\s
-# php phpunit.phar --bootstrap bootstrap.php tests/AElfTest.php
+wget https://github.com/AElfProject/AElf/releases/download/v1.0.0-preview1/aelf.zip
+Expand-Archive -Path aelf.zip -DestinationPath aelf ;
+cp scripts\aelf-node\appsettings.json  aelf\aelf\appsettings.json ;
+cp scripts\aelf-node\appsettings.MainChain.TestNet.json  aelf\aelf\appsettings.MainChain.TestNet.json ;
+cd aelf/aelf
+$job2 = Start-Job -ScriptBlock { cd D:\a\1\s\aelf\aelf; dotnet AElf.Launcher.dll; } 
+sleep 60
+cd D:\a\1\s
+php phpunit.phar --bootstrap bootstrap.php tests/AElfTest.php
