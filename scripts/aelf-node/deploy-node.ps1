9ac1fab7cff987341be7f75c952d7dcd5b2392ef
wudameinv <53204268+wudameinv@users.noreply.github.com>
2020-04-22 10:54:20 +0800

    Update deploy-node.ps1

--- scripts/aelf-node/deploy-node.ps1
+++ scripts/aelf-node/deploy-node.ps1
@@ -1,3 +1,12 @@
+wget https://github.com/microsoftarchive/redis/releases/download/win-3.2.100/Redis-x64-3.2.100.zip -OutFile  redis.zip
+Expand-Archive -Path redis.zip -DestinationPath redis ;
+cd redis
+$job1 = Start-Job -ScriptBlock { cd c:\redis; redis-server redis.windows.conf; }
+netstat -an
+get-job
+sleep 30
+get-job
+netstat -an
 mkdir -p C:\Users\VssAdministrator\AppData\Local\aelf\keys
 cp -r scripts\aelf-node\keys\* C:\Users\VssAdministrator\AppData\Local\aelf\keys;
 wget http://18.203.235.132:8000/aelf-node.zip -OutFile  aelf.zip ; 
@@ -5,9 +14,6 @@ Expand-Archive -Path aelf.zip -DestinationPath aelf ;
 cp scripts\aelf-node\appsettings.json  aelf\aelf-node\appsettings.json ;
 cp scripts\aelf-node\appsettings.MainChain.TestNet.json  aelf\aelf-node\appsettings.MainChain.TestNet.json ;
 cd aelf/aelf-node 
-# $job1 = Start-Job -ScriptBlock { cd D:\a\1\s\aelf\aelf-node; pwd; }
-# Wait-Job $job1
-# Receive-Job -Job $job1
 $job2 = Start-Job -ScriptBlock { cd D:\a\1\s\aelf\aelf-node; dotnet AElf.Launcher.dll; } 
 sleep 60
 # get-job
