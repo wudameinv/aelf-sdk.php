57e8a63aaf1b6473529863691a9c27d46bfd66fa
wudameinv <53204268+wudameinv@users.noreply.github.com>
2020-04-22 10:59:53 +0800

    Update deploy-node.ps1

--- scripts/aelf-node/deploy-node.ps1
+++ scripts/aelf-node/deploy-node.ps1
@@ -8,20 +8,14 @@ get-job
 sleep 30
 get-job
 netstat -an
-mkdir -p C:\Users\VssAdministrator\AppData\Local\aelf\keys
-cp -r scripts\aelf-node\keys\* C:\Users\VssAdministrator\AppData\Local\aelf\keys;
-wget http://18.203.235.132:8000/aelf-node.zip -OutFile  aelf.zip ; 
-Expand-Archive -Path aelf.zip -DestinationPath aelf ;
-cp scripts\aelf-node\appsettings.json  aelf\aelf-node\appsettings.json ;
-cp scripts\aelf-node\appsettings.MainChain.TestNet.json  aelf\aelf-node\appsettings.MainChain.TestNet.json ;
-cd aelf/aelf-node 
-$job2 = Start-Job -ScriptBlock { cd D:\a\1\s\aelf\aelf-node; dotnet AElf.Launcher.dll; } 
-sleep 60
-# get-job
-# Receive-Job -Job $job2
-# netstat -an
-# get-job
-# sleep 100 
-# get-job
-cd D:\a\1\s
-php phpunit.phar --bootstrap bootstrap.php tests/AElfTest.php
+# mkdir -p C:\Users\VssAdministrator\AppData\Local\aelf\keys
+# cp -r scripts\aelf-node\keys\* C:\Users\VssAdministrator\AppData\Local\aelf\keys;
+# wget http://18.203.235.132:8000/aelf-node.zip -OutFile  aelf.zip ; 
+# Expand-Archive -Path aelf.zip -DestinationPath aelf ;
+# cp scripts\aelf-node\appsettings.json  aelf\aelf-node\appsettings.json ;
+# cp scripts\aelf-node\appsettings.MainChain.TestNet.json  aelf\aelf-node\appsettings.MainChain.TestNet.json ;
+# cd aelf/aelf-node 
+# $job2 = Start-Job -ScriptBlock { cd D:\a\1\s\aelf\aelf-node; dotnet AElf.Launcher.dll; } 
+# sleep 60
+# cd D:\a\1\s
+# php phpunit.phar --bootstrap bootstrap.php tests/AElfTest.php
